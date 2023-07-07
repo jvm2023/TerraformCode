e657ac46dc895234ac6ae616f0627e847f085669
jvm2023 <125207795+jvm2023@users.noreply.github.com>
2023-07-07 17:38:22 +0530

    Update locals.tf

--- code15_random_useofsubstr_joinfunction/locals.tf
+++ code15_random_useofsubstr_joinfunction/locals.tf
@@ -2,6 +2,7 @@ locals {
   rg_name =   "${var.nameofrg}-prod"
   location = "West Europe"
   storageaccountinfo = {
+# we can also use lower function to make sure all in lowercase
     nameofsa = join("",["${var.saname}",substr(random_uuid.forsaccount.result,0,6)])
     tier = "Standard"
 
